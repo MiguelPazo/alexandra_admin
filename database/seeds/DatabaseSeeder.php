029e4d8f93cb6bb659e0f6f14b31d41a32d67af3
Miguel Rodrigo Pazo Sanchez <miguel.ps19@gmail.com>
2015-07-16 18:14:30 -0500

    delete migrations

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -17,10 +17,7 @@ class DatabaseSeeder extends Seeder
 
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
 
-        $this->call('UserSeeder');
-        $this->call('ProcessSeeder');
-        $this->call('VoterSeeder');
-        $this->call('AgrupolSeeder');
+//        $this->call('Seeder');
 
         DB::statement('SET FOREIGN_KEY_CHECKS=1;');
     }
